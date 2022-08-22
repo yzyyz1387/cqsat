f125dab90597dd891f7a0922edb54c2192251120
yzyyz1387 <1796031384@qq.com>
2022-08-23 01:35:06 +0800

    :memo: 日志工具

---
+++ cqsat/log.py
@@ -0,0 +1,32 @@
+# python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/21 2:15
+# @Author  : yzyyz
+# @Email   :  youzyyz1384@qq.com
+# @File    : log.py
+# @Software: PyCharm
+import os
+import sys
+import logging
+
+
+def log(path: str = './log.log') -> logging.Logger:
+    """
+    日志
+    :return:
+    """
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s-%(levelname)s: %(message)s')
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.formatter = formatter
+    console_handler.setLevel(logging.INFO)
+    logfile = path
+    File_handler = logging.FileHandler(logfile, mode='a', encoding='utf-8')
+    File_handler.setFormatter(formatter)
+    File_handler.setLevel(logging.DEBUG)
+    if not logger.handlers:
+        logger.addHandler(File_handler)
+        logger.addHandler(console_handler)
+        print(logger.handlers)
+    return logger
