18bef7685a78c14a52a42c71959555f3ecad53c6
yzyyz1387 <1796031384@qq.com>
2022-09-01 04:59:26 +0800

    :sparkles: 根据官方的TXT题库生成json文件（写的跟 :poop: 一样）

---
+++ pre/__init__.py
@@ -0,0 +1,7 @@
+# python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/31 4:36
+# @Author  : yzyyz
+# @Email   :  youzyyz1384@qq.com
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ pre/pre.py
@@ -0,0 +1,61 @@
+# python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/31 3:11
+# @Author  : yzyyz
+# @Email   :  youzyyz1384@qq.com
+# @File    : pre.py
+# @Software: PyCharm
+import json
+from pathlib import Path
+
+RESOURCE = Path.cwd().parent / "resource"
+BANK = RESOURCE / "bank"
+
+
+def txt2json(level: str) -> None:
+    """
+    将题库txt文件转换为json文件
+    :param level: 难度
+    :return:
+    """
+    with open(f"{level}.txt", 'r', encoding='utf-8') as f:
+        data = f.read()
+    data = data.split('\n')
+    data = list(filter(None, data))
+    count = 0
+    temp = []
+    sum_ = []
+    dic = {}
+    for i in range(len(data)):
+        print(count)
+        if count == 6:
+            temp.append(data[i])
+            count = 0
+            sum_.append(temp)
+            temp = []
+        else:
+            temp.append(data[i])
+            count += 1
+    s2 = []
+    for i in range(len(sum_)):
+        question = sum_[i]
+        temp = []
+        for j in question:
+            j = j.replace("[", "")
+            j = j.split("]")
+            temp.append(j)
+        s2.append(temp)
+    for i in range(len(s2)):
+        temp_dic = {}
+        for j in s2[i]:
+            count2 = str(i + 1)
+            temp_dic[j[0]] = j[1]
+            dic[count2] = temp_dic
+    with open(BANK / f"{level}.json", 'w', encoding='utf-8') as f:
+        json.dump(dic, f, ensure_ascii=False, indent=4)
+
+
+if __name__ == '__main__':
+    txt2json("A")
+    txt2json("B")
+    txt2json("C")
