82e02a917bd7e1f875232e6ddb5d41e52325fd33
yzyyz1387 <1796031384@qq.com>
2023-09-12 01:50:42 +0800

    :bug: Fix Metadata

--- cqsat/__init__.py
+++ cqsat/__init__.py
@@ -20,6 +20,6 @@ __plugin_meta__ = PluginMetadata(
     description="业余无线电相关工具",
     usage="可刷题、模拟考试，追踪卫星等",
     type="application",
-    homepage="https://github.com/yzyyz1387/nonebot_plugin_cqsat",
+    homepage="https://github.com/yzyyz1387/cqsat",
     supported_adapters=None,
 )
